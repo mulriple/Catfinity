d9d265558d2469380f099ab6dc766f189c76a627
Tim Johnsen <timonus@gmail.com>
2012-02-20 16:46:48 -0800

    Initial Commit

---
+++ InfiniteCats/ICAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ICAppDelegate.h
+//  InfiniteCats
+//
+//  Created by Tim Johnsen on 2/20/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ICAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ InfiniteCats/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  InfiniteCats
+//
+//  Created by Tim Johnsen on 2/20/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ICAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([ICAppDelegate class]));
+	}
+}
